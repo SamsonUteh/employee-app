59e4287bfa81d0bccea330718820c85f4c34d015
Nadus Dev <nadusdev@outlook.com>
2022-07-30 18:47:26 +0100

    added navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
+import { EditEmployeeComponent } from './employee/edit-employee/edit-employee.component';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepartmentComponent } from './department/show-department/show-department.component';
+import { EditDepartmentComponent } from './department/edit-department/edit-department.component';
+import { AddDepartmentComponent } from './department/add-department/add-department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmployeeComponent,
+    EditEmployeeComponent,
+    AddEmployeeComponent,
+    DepartmentComponent,
+    ShowDepartmentComponent,
+    EditDepartmentComponent,
+    AddDepartmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
