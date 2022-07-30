0ea32e73e5242f43e4a2b66d569049e42606a953
Nadus Dev <nadusdev@outlook.com>
2022-07-30 19:16:58 +0100

    added model and service files

---
+++ src/app/models/department-model.ts
@@ -0,0 +1,4 @@
+export class Deparatment {
+  departmentId!: number;
+  departmentName!: string;
+}
---
+++ src/app/models/employee-model.ts
@@ -0,0 +1,10 @@
+export class Employee {
+  constructor(
+    public employeeId: number,
+    public employeeName: string,
+    public department: string,
+    public maiId: string,
+    public doj: Date
+  ){}
+
+}
